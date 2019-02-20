2cf223e6c5636c628ef6b3e6f42059918e1f50b8
Stephane Eybert <mittiprovence@yahoo.se>
2019-02-20 15:15:22 +0200

    Corrected the path to use the /usr/local directory.

--- start.sh
+++ start.sh
@@ -2,8 +2,8 @@
 
 # Expand the secrets TODO
 #export DB_ROOT_PASSWORD={{DOCKER-SECRET:DB_ROOT_PASSWORD}}
-#source /usr/bin/learnintouch/expand-secrets.sh
+#source /usr/local/ngzero/expand-secrets.sh
 
 # Start the HTTP server
-/usr/bin/apache/bin/apachectl start && tail -f /dev/null
+/usr/local/apache/bin/apachectl start && tail -f /dev/null
 
