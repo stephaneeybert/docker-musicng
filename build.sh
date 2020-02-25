17e24d07d1aac600dff8f0301da0dd968e2b71d7
Stephane Eybert <mittiprovence@yahoo.se>
2020-02-25 12:52:16 +0100

    Disabled the push to the remote server.

--- build.sh
+++ build.sh
@@ -7,5 +7,5 @@ docker tag stephaneeybert/musicng-dev localhost:5000/musicng
 docker push localhost:5000/musicng
 docker build -t stephaneeybert/musicng-prod --rm=true -f Dockerfile.prod .
 docker tag stephaneeybert/musicng-prod thalasoft.com:5000/musicng
-docker push thalasoft.com:5000/musicng
+#docker push thalasoft.com:5000/musicng
 fi
