85126bc8254804aed02a9beb1c9bc35db83ed81d
Stephane Eybert <mittiprovence@yahoo.se>
2020-02-28 12:22:37 +0100

    Removed a rubish directory.

--- src/app/layouts/unsecured/unsecured.layout.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-unsecured-layout',
-  templateUrl: './unsecured.layout.component.html'
-})
-export class UnsecuredLayoutComponent { }
